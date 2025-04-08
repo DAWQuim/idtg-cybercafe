4fa2a8e08a9fed314041215a075c9e77e1767918
DAWQuim <q.vila2smixnuria2020@gmail.com>
2025-04-08 16:27:19 +0200

    Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,23 +12,34 @@
 
 
 <!-- Formulario de Login -->
-<div class="bg-black bg-opacity-70 p-8 rounded-lg text-white w-96 mt-10">
-    <h2 class="text-2xl font-bold text-center mb-4">Iniciar Sesión</h2>
-    <form method="POST" action="#" class="space-y-3" id="loginForm">
-        @csrf
-        <input type="text" class="w-full p-2 rounded bg-gray-800 text-white" name="username" placeholder="Usuario" required>
-        <input type="password" class="w-full p-2 rounded bg-gray-800 text-white" name="password" placeholder="Contraseña" required>
-        <div class="flex items-center">
-            <input type="checkbox" id="remember" name="remember" class="mr-2">
-            <label for="remember">Recuérdame</label>
-        </div>
+<div class="bg-[#2B2B2B] bg-opacity-70 min-h-[80vh] flex justify-center items-center py-10">
+    <div class="p-8 rounded-lg text-white w-96 border border-white">
+        <h2 class="text-2xl font-bold text-center mb-4">Iniciar Sesión</h2>
+            <form method="POST" action="#" class="space-y-4" id="loginForm">
+            @csrf
+            
+            <input type="text" class="w-full p-2 rounded bg-gray-800 text-white" name="username" placeholder="Usuario" required>
+
+            <input type="password" class="w-full p-2 rounded bg-gray-800 text-white" name="password" placeholder="Contraseña" required>
+
+            <div class="flex items-center">
+                <input type="checkbox" id="remember" name="remember" class="mr-2">
+                <label for="remember">Recuérdame</label>
+            </div>
+
         <button type="submit" class="w-full p-2 rounded bg-green-500 hover:bg-green-600 text-white font-bold">Iniciar Sesión</button>
     </form>
-    <p class="mt-3 text-center">
+
+    <p class="mt-4 text-center">
         <a href="{{ route('password.request') }}" class="text-green-400 hover:underline">¿Olvidaste tu contraseña?</a>
     </p>
-    <p class="text-center">¿No tienes cuenta? <a href="{{ route('register') }}" class="text-green-400 hover:underline">Regístrate</a></p>
+    <p class="text-center">
+        ¿No tienes cuenta?
+        <a href="{{ route('register') }}" class="text-green-400 hover:underline">Regístrate</a>
+    </p>
+    </div>
 </div>
+
 
 <script>
     document.getElementById("loginForm").addEventListener("submit", function(event) {
