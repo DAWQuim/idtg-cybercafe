04670e69fb9e1105663123543f9686ceedffa16a
DaniGGG627 <dgarciaguijarro1daw@gmail.com>
2025-05-12 16:23:12 +0200

    home modificado

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -34,29 +34,29 @@
 
     <!-- Areas -->
     <div id="areas" class="flex flex-col items-center py-12 gap-8 bg-[url('/public/images/cafeteria.jpg')] bg-cover">
-        <x-web-area-card>
+        <x-web-cafeteria-card>
             <x-slot:areaName>Cafeteria</x-slot>
             <x-slot:areaDesc>Un espacio diseñado para recargar energías sin salir del mundo gamer. Disfruta de café, snacks y bebidas en un ambiente moderno con iluminación RGB y pantallas con contenido en vivo. Perfecto para tomar un descanso, socializar o seguir la acción mientras comes.</x-slot>
             <x-slot:areaPicture>
                 <img src="{{ asset("images/cafeteria.jpg") }}" alt="">
             </x-slot>
-        </x-web-area-card>
+        </x-web-cafeteria-card>
 
-        <x-web-area-card>
+        <x-web-gaming-card>
             <x-slot:areaName>Sala Gaming</x-slot>
             <x-slot:areaDesc>El corazón de la experiencia: estaciones de alto rendimiento, periféricos de calidad y conexión ultrarrápida. Compite, juega en equipo o disfruta en solitario con la mejor tecnología y ambiente envolvente.</x-slot>
             <x-slot:areaPicture>
                 <img src="{{ asset("images/cafeteria.jpg") }}" alt="">
             </x-slot>
-        </x-web-area-card>
+        </x-web-gaming-card>
 
-        <x-web-area-card>
+        <x-web-coworking-card>
             <x-slot:areaName>Coworking</x-slot>
             <x-slot:areaDesc>Un espacio pensado para la productividad sin perder la esencia gamer. Con mesas cómodas, conexión estable y un ambiente inspirador, aquí puedes trabajar, estudiar o desarrollar proyectos mientras te rodeas de creatividad y tecnología.</x-slot>
             <x-slot:areaPicture>
                 <img src="{{ asset("images/cafeteria.jpg") }}" alt="">
             </x-slot>
-        </x-web-area-card>
+        </x-web-coworking-card>
     </div>
 
     <!-- Separador areas y contacto -->
