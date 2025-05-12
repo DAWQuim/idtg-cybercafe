86c8d7fb1b2be9d0f9ac013aba3816ddd07c7827
DaniGGG627 <dgarciaguijarro1daw@gmail.com>
2025-05-12 16:19:44 +0200

    vistas terminadas

--- resources/views/components/web-cafeteria-card.blade.php
+++ resources/views/components/web-cafeteria-card.blade.php
@@ -2,7 +2,7 @@
     <div class="w-full sm:w-1/2 flex flex-col gap-2 sm:gap-8">
         <h3 class="w-fit font-khand text-3xl text-black">{{ $areaName }}</h3>
         <x-web-paragraph class="w-fit !text-black">{{ $areaDesc }}</x-web-paragraph>
-        <a href="/areas" draggable="false">
+        <a href="/cafeteria" draggable="false">
             <x-web-button class="text-base tracking-widest">Saber Mas <img src="{{ asset("images/right-arrow-svgrepo-com.svg") }}" class="h-4 inline fill-black"></img></x-web-button>
         </a>
     </div>
@@ -10,4 +10,3 @@
         {{ $areaPicture }}
     </div>
 </div>
-
