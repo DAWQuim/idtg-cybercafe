9ea08fee8ca015f6817c8eb01dfa22e38ad4333d
Saerujii <sergeysmik.yt@gmail.com>
2025-05-13 17:20:14 +0200

    Web smooth scroll

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="scroll-smooth">
 
 <head>
     <meta charset="utf-8">
@@ -33,7 +33,7 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class="font-sans antialiased scroll-smooth">
+<body class="font-sans antialiased">
     <x-header />
 
     <!-- Page Content -->
